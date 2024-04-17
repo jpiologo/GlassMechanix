0cfa5926bf85535db01905cd89d6509a1873ed10
João Piologo <122281207+jpiologo@users.noreply.github.com>
2024-04-17 11:25:46 -0300

    Add files via upload

--- scripts/whatsapp.js
+++ scripts/whatsapp.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("contact-button").addEventListener("click", function() {
         var phoneNumber = "5541991793386";
-        var message = "Olá! Gostaria de conversar sobre Reparo/Cristalização.";
+        var message = "Olá! Gostaria de conversar sobre Reparo/Cristalização/Polimento.";
         var whatsappLink = "https://wa.me/" + phoneNumber + "?text=" + encodeURIComponent(message);
         window.open(whatsappLink, '_blank');
     });
